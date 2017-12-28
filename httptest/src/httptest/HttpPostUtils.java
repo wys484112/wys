4fa15b73a50fa9ac65756e8fbe521d36fedfd21d
wys484112 <wys484112@163.com>
2017-12-28 17:12:13 +0800

    http visit style

---
+++ httptest/src/httptest/HttpPostUtils.java
@@ -0,0 +1,137 @@
+package httptest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+/**
+ * Created by 桐木木 on 2017/2/8.
+ * 发送Http请求，获取Controller返回的数据
+ *
+ */
+
+public class HttpPostUtils {
+    public static String doPostRequest(String path,Object content){
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String resoult = "";
+        try {
+            System.out.println("要发送的信息是："+content);
+            /*拼接url,Android这里需要换上远程地址，因为Android端没办法访问localhost，java的话本地tomcat运行的话倒是无妨*/
+            String address = "http://localhost:8080/"+path;
+            URL url = new URL(address);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            //这两个参数必须加
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            //设置超时时间
+            httpURLConnection.setReadTimeout(10*1000);
+            httpURLConnection.setConnectTimeout(10*1000);
+            httpURLConnection.setRequestMethod("POST");
+//            httpURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");   
+            
+            httpURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");   
+            
+            httpURLConnection.connect();
+
+            out = new PrintWriter(httpURLConnection.getOutputStream());
+            //在输出流中写入参数
+            out.print(content);
+            out.flush();
+
+            if(httpURLConnection.getResponseCode() == 200){
+                in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(),"utf-8"));
+                String line;
+                while((line=in.readLine())!=null){
+                    resoult+=line;
+                }
+            }
+            System.out.println("服务器返回的结果是："+resoult);
+            return resoult;
+        } catch (MalformedURLException e) {
+            System.out.println("URL异常");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO异常");
+            e.printStackTrace();
+        }finally {
+            try{
+                if(out!=null)
+                    out.close();
+                if(in!=null)
+                    in.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+    
+    
+    public static String doJsonPostRequest(String path,JSONObject content){
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String resoult = "";
+        try {
+            System.out.println("要发送的信息是："+content);
+            /*拼接url,Android这里需要换上远程地址，因为Android端没办法访问localhost，java的话本地tomcat运行的话倒是无妨*/
+            String address = "http://localhost:8080/iot/"+path;
+            URL url = new URL(address);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            //这两个参数必须加
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            //设置超时时间
+            httpURLConnection.setReadTimeout(10*1000);
+            httpURLConnection.setConnectTimeout(10*1000);
+            httpURLConnection.setRequestMethod("POST");
+//            httpURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");   
+            
+            httpURLConnection.setRequestProperty("Content-Type","application/json");   
+            
+            httpURLConnection.connect();
+
+            out = new PrintWriter(httpURLConnection.getOutputStream());
+            //在输出流中写入参数
+            String content1 = content.toString();//java.net.URLEncoder.encode(content.toString(), "utf-8");
+			System.out.println("服务器doJsonPostRequest返回的数据为：" + content1);
+
+            out.print(content1);
+            out.flush();
+
+            if(httpURLConnection.getResponseCode() == 200){
+                in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(),"utf-8"));
+                String line;
+                while((line=in.readLine())!=null){
+                    resoult+=line;
+                }
+            }
+            System.out.println("服务器返回的结果是："+resoult);
+            return resoult;
+        } catch (MalformedURLException e) {
+            System.out.println("URL异常");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO异常");
+            e.printStackTrace();
+        }finally {
+            try{
+                if(out!=null)
+                    out.close();
+                if(in!=null)
+                    in.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
---
+++ httptest/src/httptest/httptest.java
@@ -0,0 +1,76 @@
+package httptest;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.json.JSONObject;
+
+public class httptest {
+
+	public static void main(String[] args) {
+		
+//		
+//		String test =  "username=吴寅生&password=111111";
+//		try {
+//			test="username="+URLEncoder.encode("吴寅生", "utf-8")+"&password=111111";
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String ans = HttpPostUtils.doPostRequest("login.action", test);
+//		System.out.println("服务器返回的数据为：" + ans);
+//		
+		
+		
+//		User user=new User();
+//		user.setId(1);
+//		try {
+//			user.setName(URLEncoder.encode("吴寅生", "utf-8"));
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		user.setName("111111");
+//
+//		user.setProductid(333);
+//		
+//		user.setPassword("aaaa");
+//		  JSONObject object = (JSONObject) JSONObject.wrap(user);
+//			System.out.println("User：" + object.toString());
+//			
+//			String aaaa = HttpPostUtils.doJsonPostRequest("testjson.action", object);
+//			System.out.println("服务器返回的数据为：" + aaaa);
+			
+			
+			
+			
+			
+			User user=new User();
+			user.setId(1);
+			try {
+				user.setName(URLEncoder.encode("吴寅生", "utf-8"));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			user.setName("111111");
+
+			user.setProductid(333);
+			
+			user.setPassword("aaaa");
+			  JSONObject object = (JSONObject) JSONObject.wrap(user);
+				System.out.println("User：" + object.toString());
+				
+				String aaaa = null;
+				try {
+					aaaa = HttpPostUtils.doPostRequest("login", "username="+URLEncoder.encode("吴寅生", "utf-8")+"&password=111111");
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("服务器返回的数据为：" + aaaa);
+				
+				
+	}
+
+}
